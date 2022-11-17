1d6def1f6e2865ea79715070548130123c408b46
RAGHAV70147 <E21CSEU0501@bennett.edu.in>
2022-11-17 14:43:22 +0530

    J UNIT TEST

---
+++ src/jUnitTestPackage/jUnitFunctions.java
@@ -0,0 +1,10 @@
+package jUnitTestPackage;
+
+public class jUnitFunctions {
+	public int testAddNumbers(int n1, int n2) {
+		return n1 + n2;
+	}
+	public String testAddStrings(String s1, String s2) {
+		return s1 + s2;
+	}
+}
